eb46e4a1d705e91ce7a54c95b000fe725df4f7aa
Jonathan Hall <flimzy@flimzy.com>
2017-07-24 18:39:41 +0200

    Switch covermode, needed for Go 1.9

--- travis/test.sh
+++ travis/test.sh
@@ -2,7 +2,7 @@
 set -euC
 set -o xtrace
 
-go test -race -cover -covermode=set -coverprofile=coverage.txt
+go test -race -cover -covermode=atomic -coverprofile=coverage.txt
 
 # Only continue if we're on go 1.8; no need to run the linter for every case
 if go version | grep -q go1.8; then
